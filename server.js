e1577e97374f3568771f64185431467df7f16bf4
Finn Terdal <finnterdal@gmail.com>
2023-02-13 23:34:12 -0300

    adds a kitten server

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import data from "./data.json" assert { type: "json" };
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("hello there! you can use this api to retrieve some kittens");
+});
+
+app.get("/api/kittens", (req, res) => {
+  res.send(data);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
