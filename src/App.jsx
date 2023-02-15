5ed0311ef8ae5041c0d7b8c2dd4457d6e0132970
Finn Terdal <finnterdal@gmail.com>
2023-02-15 15:04:26 -0300

    finished demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-// import Counter from "./Counter";
-
 function App() {
+  const [kittens, setKittens] = useState([]);
+
+  useEffect(() => {
+    async function doFetch() {
+      const response = await fetch("http://localhost:8080/api/kittens");
+      const data = await response.json();
+      setKittens(data);
+      console.log("data", data);
+    }
+    doFetch();
+  }, []);
+
   return (
     <>
       <h1>😸 Kittens 😻</h1>
-      <div className="kitten-collection">
-        <div className="kitten">
-          <h2>Timothy</h2>
-          <img
-            className="kitten-pic"
-            src="https://robohash.org/Timothy?set=set4"
-          />
+      {kittens.map((kitten) => (
+        <div key={kitten.id} className="kitten-collection">
+          <div className="kitten">
+            <h2>{kitten.name}</h2>
+            <img
+              className="kitten-pic"
+              src={kitten.image}
+            />
+          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
