17e901ff0c87b3b27e9c9e8a200f9e3d283579e9
Finn Terdal <finnterdal@gmail.com>
2023-02-14 23:40:40 -0300

    reasonable demo stop point

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
-import data from "./data.json" assert { type: "json" };
+// import data from "./data.json" assert { type: "json" };
+import { readFileSync } from "fs";
+const data = JSON.parse(readFileSync("./data.json"));
 
 const app = express();
 
@@ -12,6 +14,9 @@ app.get("/", (req, res) => {
 
 app.get("/api/kittens", (req, res) => {
   res.send(data);
+  // setTimeout(() => {
+  // }, 3000);
+  // res.sendStatus(500);
 });
 
 const PORT = 8080;
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// import Counter from "./Counter";
 
-  function incrementCount() {
-    setCount(count + 1);
-  }
+function App() {
+  const [kittens, setKittens] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/kittens")
+      .then((response) => response.json())
+      .then((data) => {
+        setKittens(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>
-      <h2>🧛🏻‍♂️ Click to Count 👇🏻</h2>
-      <button onClick={incrementCount}>count is {count}</button>
+      <h1>😸 Kittens 😻</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Uh oh! Something went wrong. Please try again later.</p>}
+      <div className="kitten-collection">
+        {kittens.map((kitten) => (
+          <div key={kitten.id} className="kitten">
+            <h2>{kitten.name}</h2>
+            <img
+              className="kitten-pic"
+              style={{ maxWidth: "7rem" }}
+              src={kitten.image}
+            />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
